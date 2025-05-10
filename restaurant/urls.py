c1ee421840505ab3d64ab349ab77afc6241bc690
Mike Patterson <mppatterson84@gmail.com>
2025-05-10 12:25:20 -0500

    update route to menu-items

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,6 +4,6 @@ from restaurant import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('', views.home, name='home'),
-    path('menu/', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view()),
 ]
